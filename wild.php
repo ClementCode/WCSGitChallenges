50dc82392d837508d4ba31a73c112005f4b74ec5
ClementCode <dubeaurepaire59@gmail.com>
2018-10-08 12:09:01 +0200

    A la Wild Code School on code en chaussettes.

---
+++ wild.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 08/10/18
+ * Time: 11:57
+ */
+
+/* Coucou mes très chers amis Wilders,
+   J'espère que pour vous aussi, ce challenge est très simple à réaliser.
+   Vous êtes en train d'apprendre le début de votre outil de tous les jours
+   Et ce n'est que le début....
+
+   Bonne journée.
+   Et vive la Wild */
+
+/**
+ * Pour votre culture ami PHP, ce type d'annotation vient de Symfony.
+ * A ne pas vous planter avec des commentaires classiques (ci-dessus)
+ */
